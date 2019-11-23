c300e09eea6574c8f924ff932022779088111483
adolfomunoz <adolfo@unizar.es>
2019-11-23 01:22:02 +0100

    Updated compilation.

---
+++ External.cmake
@@ -0,0 +1,121 @@
+##################################################################################
+# LIBPNG
+##################################################################################
+find_package(PNG)
+if(PNG_FOUND)
+	include_directories(${PNG_INCLUDE_DIR})
+	list(APPEND cimg_libs ${PNG_LIBRARY})
+	list(APPEND cimg_defs "cimg_use_png")
+	list(APPEND svg_cpp_plot_libs ${PNG_LIBRARY})
+	list(APPEND svg_cpp_plot_defs "USE_PNG")
+endif(PNG_FOUND)
+
+find_package(JPEG)
+if(JPEG_FOUND)
+	include_directories(${JPEG_INCLUDE_DIR})
+	list(APPEND cimg_libs ${JPEG_LIBRARY})
+	list(APPEND cimg_defs "cimg_use_jpeg")
+endif(JPEG_FOUND)
+
+find_package(TIFF)
+if(TIFF_FOUND)
+	include_directories(${TIFF_INCLUDE_DIR})
+	list(APPEND cimg_libs ${TIFF_LIBRARY})
+	list(APPEND image_defs "cimg_use_tiff")
+endif(TIFF_FOUND)
+
+if(FFTW3_FOUND)
+	include_directories(${FFTW3_INCLUDE_DIR})
+	list(APPEND cimg_libs ${FFTW3_LIBRARY})
+	list(APPEND cimg_defs "cimg_use_fftw3")
+endif(FFTW3_FOUND)
+
+#find_package(ImageMagick COMPONENTS Magick++)
+#if (ImageMagick_FOUND)
+#	include_directories(${ImageMagick_INCLUDE_DIRS})
+#	list(APPEND cimg_libs ${ImageMagick_LIBRARIES})
+#	list(APPEND cimg_defs "cimg_use_magick")
+#	list(APPEND cimg_defs "MAGICKCORE_QUANTUM_DEPTH=16")
+#	list(APPEND cimg_defs "MAGICKCORE_HDRI_ENABLE=0")
+#endif(ImageMagick_FOUND)
+
+find_package(Eigen3 REQUIRED)
+include_directories(${EIGEN3_INCLUDE_DIR})
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    # Windows specific code
+    list(APPEND cimgdisplay_libs gdi32)
+    list(APPEND cimgdisplay_defs cimg_display=2)
+    list(APPEND cimg_defs cimg_OS=2)
+else(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    list(APPEND cimg_libs pthread)
+    list(APPEND cimgdisplay_libs X11)
+    list(APPEND cimgdisplay_defs cimg_display=1)
+    list(APPEND cimg_defs cimg_OS=1)
+endif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+
+
+######################################################################
+# EXTERNAL LIBRARIES
+######################################################################
+if (NOT EXTERNAL_INSTALL_LOCATION)
+	set(EXTERNAL_INSTALL_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/external)
+endif()
+if (NOT IS_DIRECTORY ${EXTERNAL_INSTALL_LOCATION})
+	file(MAKE_DIRECTORY ${EXTERNAL_INSTALL_LOCATION})
+endif()
+
+option(BUILD_OFFLINE "Build offline" OFF)
+
+# If offline you can set -DBUILD_OFFLINE=ON so it does not try to update stuff
+if (${BUILD_OFFLINE})
+	set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                 PROPERTY EP_UPDATE_DISCONNECTED 1)
+endif()
+
+
+include(ExternalProject)
+# External include directory
+include_directories(${EXTERNAL_INSTALL_LOCATION})
+
+ExternalProject_Add(cimg
+  GIT_REPOSITORY https://framagit.org/dtschump/CImg.git 
+  SOURCE_DIR ${EXTERNAL_INSTALL_LOCATION}/CImg
+  BUILD_COMMAND ""
+  CONFIGURE_COMMAND ""
+  INSTALL_COMMAND ""
+)
+
+ExternalProject_Add(cimg-additions
+  GIT_REPOSITORY https://github.com/adolfomunoz/cimg_additions.git
+  SOURCE_DIR ${EXTERNAL_INSTALL_LOCATION}/cimg_additions
+  BUILD_COMMAND ""
+  CONFIGURE_COMMAND ""
+  INSTALL_COMMAND ""
+)
+
+ExternalProject_Add(eigen
+  GIT_REPOSITORY https://github.com/eigenteam/eigen-git-mirror
+  SOURCE_DIR ${EXTERNAL_INSTALL_LOCATION}/eigen
+  BUILD_COMMAND ""
+  CONFIGURE_COMMAND ""
+  INSTALL_COMMAND ""
+)
+
+ExternalProject_Add(catch
+  GIT_REPOSITORY https://github.com/philsquared/Catch.git
+  SOURCE_DIR ${EXTERNAL_INSTALL_LOCATION}/catch
+  BUILD_COMMAND ""
+  CONFIGURE_COMMAND ""
+  INSTALL_COMMAND ""
+)
+
+include_directories(${EXTERNAL_INSTALL_LOCATION}/CImg)
+include_directories(${EXTERNAL_INSTALL_LOCATION}/cimg_additions)
+include_directories(${EXTERNAL_INSTALL_LOCATION}/eigen)
+include_directories(${EXTERNAL_INSTALL_LOCATION}/catch/single_include/catch2)
+
+
+
+
+
